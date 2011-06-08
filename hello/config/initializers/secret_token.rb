40f5d0bc428aabc43667f7f6650160550c86acb1
Luis Michel <luis.michel@binah.mx>
2011-06-07 19:47:39 -0500

    Se agregaron todo el esqueleto del proyecto de Rails y un hello world

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '8d659967ca3b19427321fa21be4bd56d344654e56854290280987ae5ae542a5687aa044e7dba3fc6053bc173842469bb432e50d8182e1ec374ee928aeef44df7'
